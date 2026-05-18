6e5f389ad52b9c5b7a83318f9c8dc9d84ce819b4
boom3146 <t7yjcv5txj@privaterelay.appleid.com>
2026-05-18 19:21:23 +0300

    Add RectangleShape class extending ParallelogramShape

---
+++ RectangleShape.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace final_project
+{
+    public class RectangleShape : ParallelogramShape
+    {
+        public RectangleShape(int x, int y, Color shapeColor, int height, int width): base(x, y, shapeColor, 0, height, width) { }
+
+        public override void Draw(Graphics g)
+        {
+           base.Draw(g);
+        }
+
+        public override bool IsPointInside(int mouseX, int mouseY)
+        {
+            return base.IsPointInside(mouseX, mouseY);
+        }
+    }
+}
