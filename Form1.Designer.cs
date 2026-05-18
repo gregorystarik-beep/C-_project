f8d1893af521bbcfa92ad27fcfba48d4c0645c03
boom3146 <t7yjcv5txj@privaterelay.appleid.com>
2026-05-18 19:23:37 +0300

    Add panel and radio buttons to Form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace C__project
+namespace C__project
 {
     partial class Form1
     {
@@ -32,7 +32,16 @@
             pictureBox1 = new PictureBox();
             button2 = new Button();
             button3 = new Button();
+            panel1 = new Panel();
+            radioButton4 = new RadioButton();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -55,6 +64,7 @@
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseClick += pictureBox1_MouseClick;
             // 
             // button2
             // 
@@ -65,6 +75,7 @@
             button2.TabIndex = 2;
             button2.Text = "save";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
@@ -77,11 +88,108 @@
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(radioButton4);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(997, 308);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(141, 294);
+            panel1.TabIndex = 4;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Font = new Font("Segoe UI", 18F);
+            radioButton4.Location = new Point(17, 126);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(76, 36);
+            radioButton4.TabIndex = 6;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Line";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.DarkBlue;
+            button6.Location = new Point(33, 250);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Black;
+            button5.Location = new Point(33, 221);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Yellow;
+            button4.Location = new Point(33, 192);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 18F);
+            radioButton3.Location = new Point(17, 84);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(91, 36);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Circle";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 10F);
+            radioButton2.Location = new Point(17, 55);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(110, 23);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Parallelogram";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 14F);
+            radioButton1.Location = new Point(17, 20);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(112, 29);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Rectangle";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1185, 635);
+            Controls.Add(panel1);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(pictureBox1);
@@ -90,6 +198,8 @@
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -99,5 +209,13 @@
         private PictureBox pictureBox1;
         private Button button2;
         private Button button3;
+        private Panel panel1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button4;
+        private Button button6;
+        private Button button5;
+        private RadioButton radioButton4;
     }
 }
