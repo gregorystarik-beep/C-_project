4a0b1353bc2b8fb385fc8374f47f201cbde65a74
boom3146 <t7yjcv5txj@privaterelay.appleid.com>
2026-05-18 19:23:11 +0300

    Add BaseShape class with properties and methods

---
+++ BaseShape.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace final_project
+{
+    [JsonPolymorphic]
+    [JsonDerivedType(typeof(CircleShape), typeDiscriminator: "Circle")]
+    [JsonDerivedType(typeof(RectangleShape), typeDiscriminator: "Rectangle")]
+    [JsonDerivedType(typeof(ParallelogramShape), typeDiscriminator: "Parallelogram")]
+    public abstract class BaseShape
+    {
+        public int X
+        {
+            get; set; 
+        }
+        public int Y
+        {
+            get; set;
+        }
+        public Color ShapeColor
+        {
+            get; set; 
+        }
+        public BaseShape(int x, int y,  Color shapeColor)
+        {
+            X=x;
+            Y=y;
+            ShapeColor=shapeColor;
+        }
+        public abstract void Draw(Graphics g);
+
+        public abstract bool IsPointInside(int mouseX, int mouseY);
+    }
+}
